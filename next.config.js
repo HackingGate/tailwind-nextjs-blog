59b6216777d17b23aaf52b20c123359bf8223e56
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-08-19 12:05:40 +0900

    Update siteMetadata.js
    Install "@octokit/rest" and "github-slugger"
    
    Commit pnpm-lock.yaml
    
    Move "@octokit/rest" to devDependencies
    
    Add fetch author readme script
    
    Try try catch
    
    Edit author and ran pnpm fetchdata
    
    Add workaround to unsupported features
    
    Fetch avatar from GitHub
    
    Add description text about content generation
    
    Ran pnpm fetchdata
    
    Ran git rm --cached pnpm-lock.yaml to fix Vercel build
    
    Revert "Ran git rm --cached pnpm-lock.yaml to fix Vercel build"
    
    This reverts commit a62f8bc4d76bc3aa23a95a08ac767048d5c1fe1e.
    
    Get githubUsername form URL
    
    Change about description
    
    Update default.mdx
    
    Make fetchAuthorReadme part of prebuild
    
    Move fetchAuthorReadme() to postbuild
    
    pnpm i --save-dev node-fetch
    
    Revert "Move fetchAuthorReadme() to postbuild"
    
    This reverts commit b65bba6a6f074914f123338f1f17ab0bb6afdfd4.
    
    Revert "Make fetchAuthorReadme part of prebuild"
    
    This reverts commit 5005760cf9bae24157ffdcc09f1c3abd83179028.
    
    Ran lint
    
    Add migrateFromJekyll.mjs
    
    Remove all blog posts
    
    Remove unused images
    
    Install axios as dev dep and change back to yarn
    
    Handle image migration
    
    Run blog posts migrate
    
    Fix external image filename not converted
    
    Refactor script
    
    Rerun migrate from Jekyll

--- next.config.js
+++ next.config.js
@@ -84,5 +84,104 @@ module.exports = () => {
 
       return config
     },
+    async redirects() {
+      return [
+        {
+          source: '/2015/08/31/remap-keys-in-os-x-tutorial-for-emacs-lovers.html',
+          destination: '/blog/remap-keys-in-os-x-tutorial-for-emacs-lovers',
+          permanent: true,
+        },
+        {
+          source: '/2015/09/03/xcode-project-build-number-use-svn-revision.html',
+          destination: '/blog/xcode-project-build-number-use-svn-revision',
+          permanent: true,
+        },
+        {
+          source: '/2015/09/16/helloworld-docker-on-osx-without-boot2docker.html',
+          destination: '/blog/helloworld-docker-on-osx-without-boot2docker',
+          permanent: true,
+        },
+        {
+          source: '/2015/10/01/how-to-download-xcode-with-aria2c.html',
+          destination: '/blog/how-to-download-xcode-with-aria2c',
+          permanent: true,
+        },
+        {
+          source: '/2018/01/24/started-to-learn-korean.html',
+          destination: '/blog/started-to-learn-korean',
+          permanent: true,
+        },
+        {
+          source: '/2019/03/31/macos-mojave-s-dark-mode-for-jekyll-default-theme-minima.html',
+          destination: '/blog/macos-mojave-s-dark-mode-for-jekyll-default-theme-minima',
+          permanent: true,
+        },
+        {
+          source: '/2019/06/09/deploy-jekyll-blog-on-gitlab-pages-with-cloudflare-origin-ca.html',
+          destination: '/blog/deploy-jekyll-blog-on-gitlab-pages-with-cloudflare-origin-ca',
+          permanent: true,
+        },
+        {
+          source: '/2019/06/10/resign-ipas-with-fastlane-for-jailbreak.html',
+          destination: '/blog/resign-ipas-with-fastlane-for-jailbreak',
+          permanent: true,
+        },
+        {
+          source: '/2019/06/11/inspect-the-view-hierarchy-of-any-ios-apps-on-ios-12.html',
+          destination: '/blog/inspect-the-view-hierarchy-of-any-ios-apps-on-ios-12',
+          permanent: true,
+        },
+        {
+          source: '/2019/06/12/apps-fail-to-get-wi-fi-info-on-ios-13.html',
+          destination: '/blog/apps-fail-to-get-wi-fi-info-on-ios-13',
+          permanent: true,
+        },
+        {
+          source: '/2019/06/24/upgrade-to-raspbian-buster-debian-10-for-raspberry-pi.html',
+          destination: '/blog/upgrade-to-raspbian-buster-debian-10-for-raspberry-pi',
+          permanent: true,
+        },
+        {
+          source: '/2019/07/09/using-gpg-to-encrypt-messages.html',
+          destination: '/blog/using-gpg-to-encrypt-messages',
+          permanent: true,
+        },
+        {
+          source: '/2019/07/10/gnu-linux-tar-command-cheat-sheet.html',
+          destination: '/blog/gnu-linux-tar-command-cheat-sheet',
+          permanent: true,
+        },
+        {
+          source: '/2019/07/28/using-aircrack-ng-with-rtl8814au.html',
+          destination: '/blog/using-aircrack-ng-with-rtl8814au',
+          permanent: true,
+        },
+        {
+          source: '/2020/01/25/build-openwrt-19-07-for-tl-wr703n.html',
+          destination: '/blog/build-openwrt-19-07-for-tl-wr703n',
+          permanent: true,
+        },
+        {
+          source: '/2021/03/28/started-to-learn-russian.html',
+          destination: '/blog/started-to-learn-russian',
+          permanent: true,
+        },
+        {
+          source: '/2021/03/28/started-to-learn-russian.html',
+          destination: '/blog/started-to-learn-russian',
+          permanent: true,
+        },
+        {
+          source: '/2021/08/15/network-layer-ip-address-blocking.html',
+          destination: '/blog/network-layer-ip-address-blocking',
+          permanent: true,
+        },
+        {
+          source: '/2021/12/05/moving-to-new-yubikeys.html',
+          destination: '/blog/moving-to-new-yubikeys',
+          permanent: true,
+        },
+      ]
+    },
   })
 }
