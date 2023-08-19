dd04f4a4746f7e32f7417d82091614c069db2fbd
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-08-19 18:52:21 +0900

    Add fetch author readme script

---
+++ scripts/fetchAuthorReadme.mjs
@@ -0,0 +1,43 @@
+// Fetch the README.md file from the author's GitHub repository.
+// And append the README.md file to data/authors/{author}.mdx
+
+import { promises as fs } from 'fs'
+import path from 'path'
+import { Octokit } from '@octokit/rest'
+import GithubSlugger from 'github-slugger'
+import { allAuthors } from '../.contentlayer/generated/Authors/_index.mjs'
+import fetch from "node-fetch";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+const fetchAuthorReadme = async () => {
+  const authors = allAuthors.filter((author) => author.github)
+  for (const author of authors) {
+    const { data } = await octokit.repos.getContent({
+      owner: author.github,
+      repo: author.github,
+      path: 'README.md',
+      request: {
+        fetch: fetch
+      },
+    })
+
+    const content = Buffer.from(data.content, 'base64').toString()
+    const slug = GithubSlugger.slug(author.name)
+    // Rewrite the mdx file
+    const mdx = `---
+name: ${author.name}
+github: ${author.github}
+avatar: ${author.avatar}
+email: ${author.email}
+---
+${content}
+`
+    const filepath = path.join(process.cwd(), 'data', 'authors', `${slug}.mdx`)
+    await fs.writeFile(filepath, mdx)
+  }
+}
+
+export default fetchAuthorReadme
