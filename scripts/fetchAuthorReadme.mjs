ad916469625aaf0a17f42868d652cd0239a61aad
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-08-19 12:05:40 +0900

    Update siteMetadata.js
    Install "@octokit/rest" and "github-slugger"
    
    Commit pnpm-lock.yaml
    
    Move "@octokit/rest" to devDependencies
    
    Add fetch author readme script
    
    Try try catch
    
    Edit author and ran pnpm fetchdata
    
    Add workaround to unsupported features
    
    Fetch avatar from GitHub
    
    Add description text about content generation
    
    Ran pnpm fetchdata
    
    Ran git rm --cached pnpm-lock.yaml to fix Vercel build
    
    Revert "Ran git rm --cached pnpm-lock.yaml to fix Vercel build"
    
    This reverts commit a62f8bc4d76bc3aa23a95a08ac767048d5c1fe1e.
    
    Get githubUsername form URL
    
    Change about description
    
    Update default.mdx
    
    Make fetchAuthorReadme part of prebuild
    
    Move fetchAuthorReadme() to postbuild
    
    pnpm i --save-dev node-fetch
    
    Revert "Move fetchAuthorReadme() to postbuild"
    
    This reverts commit b65bba6a6f074914f123338f1f17ab0bb6afdfd4.
    
    Revert "Make fetchAuthorReadme part of prebuild"
    
    This reverts commit 5005760cf9bae24157ffdcc09f1c3abd83179028.
    
    Ran lint
    
    Add migrateFromJekyll.mjs
    
    Remove all blog posts
    
    Remove unused images
    
    Install axios as dev dep and change back to yarn
    
    Handle image migration
    
    Run blog posts migrate
    
    Fix external image filename not converted
    
    Refactor script
    
    Rerun migrate from Jekyll

---
+++ scripts/fetchAuthorReadme.mjs
@@ -0,0 +1,90 @@
+// Fetch the README.md file from the author's GitHub repository.
+// And append the README.md file to data/authors/{author}.mdx
+
+import { promises as fs } from 'fs'
+import path from 'path'
+import { Octokit } from '@octokit/rest'
+import { allAuthors } from '../.contentlayer/generated/Authors/_index.mjs'
+import fetch from 'node-fetch'
+
+const octokit = new Octokit()
+
+const fetchAuthorReadme = async () => {
+  const authors = allAuthors.filter((author) => author.github)
+  for (const author of authors) {
+    // TBD: Make the for loop continue even if one of the requests fails
+
+    // Get GitHub username from GitHub URL
+    const githubUsername = author.github.split('/').pop()
+
+    try {
+      const { data } = await octokit.repos.getContent({
+        owner: githubUsername,
+        repo: githubUsername,
+        path: 'README.md',
+        request: {
+          fetch: fetch,
+        },
+      })
+
+      // Fetch avatar from GitHub
+      const avatar = await octokit.users.getByUsername({
+        username: githubUsername,
+        request: {
+          fetch: fetch,
+        },
+      })
+      const avatarData = await fetch(avatar.data.avatar_url)
+      // Overwrite avatar to the path of author.avatar
+      await fs.writeFile(
+        path.join(process.cwd(), 'public', author.avatar),
+        await avatarData.buffer()
+      )
+
+      // TBD: Make the MDX fully compatible with GitHub Flavored Markdown
+      // Workaround: Remove unsupported features
+      let content = Buffer.from(data.content, 'base64').toString()
+      content = removeUnsupportedFeatures(content)
+
+      // Rewrite the mdx file
+      const mdx = `---
+name: ${author.name}
+avatar: ${author.avatar}
+email: ${author.email}
+github: ${author.github}
+---
+${content}
+[See full README.md](${data.html_url})  
+`
+
+      const filepath = path.join(process.cwd(), 'data', 'authors', `${author.slug}.mdx`)
+      await fs.writeFile(filepath, mdx)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+const removeUnsupportedFeatures = (content) => {
+  let contentToReturn = content
+
+  // Remove HTML comments
+  const regex = /<!--[\s\S]*?-->/g
+  contentToReturn = contentToReturn.replace(regex, '')
+
+  // Remove badges
+  const regex2 = /\[!\[.*\]\(.*\)\]\(.*\)/g
+  contentToReturn = contentToReturn.replace(regex2, '')
+
+  // Remove HTML tags
+  const regex3 = /<[^>]*>/g
+  contentToReturn = contentToReturn.replace(regex3, '')
+
+  // Remove empty lines
+  const regex4 = /^\s*[\r\n]/gm
+  contentToReturn = contentToReturn.replace(regex4, '')
+
+  return contentToReturn
+}
+
+export default fetchAuthorReadme
---
+++ scripts/migrateFromJekyll.mjs
@@ -0,0 +1,188 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+import axios from 'axios'
+
+// Fill the path to your Jekyll repository
+const jekyllRepositoryPath = ''
+
+const migrateFromJekyll = async () => {
+  const allPosts = await fs.readdir(path.join(jekyllRepositoryPath, '_posts'))
+
+  let redirects = []
+
+  // Convert all posts and save them to the data/blog folder
+  for (const post of allPosts) {
+    // Skip unsupported posts
+    if (post === '2018-01-22-welcome-to-jekyll.markdown') {
+      continue
+    }
+
+    // Remove YYYY-MM-DD- from the filename and replace extension with .mdx
+    const filename = post.replace(/^\d{4}-\d{2}-\d{2}-/, '').replace(/\.[^/.]+$/, '.mdx')
+
+    const filepath = path.join(process.cwd(), 'data', 'blog', `${filename}`)
+
+    // If the file already exists, skip it
+    const fileExists = await fs.readFile(filepath).catch(() => false)
+    if (fileExists) {
+      console.log(`File ${filepath} already exists, skipping...`)
+      continue
+    }
+
+    const { title, date, tags, compatibleData } = await readJekyllPost(post)
+
+    const mdx = `---
+title: ${title}
+date: ${date}
+tags: ${tags}
+---
+${compatibleData}
+`
+    // Remove empty lines between frontmatter and content
+    const cleanMDX = mdx.replace(/---\n\n/, '---\n')
+    console.log(cleanMDX)
+
+    // Save the content to the file
+    await fs.writeFile(filepath, mdx)
+
+    // Append redirects to next.config.js since I moved from Jekyll to Next.js
+    // Example:
+    // module.exports = {
+    //   async redirects() {
+    //     return [
+    //       {
+    //         source: '/2021/08/15/network-layer-ip-address-blocking.html',
+    //         destination: '/blog/network-layer-ip-address-blocking', // Matched parameters can be used in the destination
+    //         permanent: true,
+    //       },
+    //     ]
+    //   },
+    // }
+
+    // Replace 'YYYY-MM-DD' with YYYY/MM/DD in the URL
+    const dateInURL = date.replace(/'/g, '').replace(/-/g, '/')
+    const redirect = `{
+        source: '/${dateInURL}/${filename.replace(/\.mdx$/, '.html')}',
+        destination: '/blog/${filename.replace(/\.mdx$/, '')}',
+        permanent: true,
+      }`
+    redirects.push(redirect)
+  }
+
+  console.log('Append the following to next.config.js')
+  const redirectsString = redirects.join(',\n      ')
+
+  console.log(`module.exports = {
+  async redirects() {
+    return [
+      ${redirectsString}
+    ]
+  },
+}`)
+}
+
+const readJekyllPost = async (post) => {
+  const postPath = path.join(jekyllRepositoryPath, '_posts', post)
+  const data = await fs.readFile(postPath, 'utf8')
+
+  // Read form Jekyll frontmatter
+  // Example:
+  // ---
+  // title: "Hello World"
+  // date: 2020-01-01
+  // tag: osx linux docker or categories: jekyll update
+  // ---
+  const title = data.match(/title: (.*)/)[1]
+  const date = `'${post.match(/^\d{4}-\d{2}-\d{2}/)[0]}'`
+  let tag = ''
+  let tagMatch = data.match(/tag: (.*)/)
+  if (tagMatch) {
+    tag = tagMatch[1]
+  } else {
+    tagMatch = data.match(/categories: (.*)/)
+    if (tagMatch) {
+      tag = tagMatch[1]
+    }
+  }
+  // convert tag to array
+  const tags = `[${tag
+    .split(' ')
+    .map((t) => `${t}`)
+    .join(', ')}]`
+
+  const cleanData = data.replace(/---\n(.|\n)*---\n/, '')
+
+  const imageConvertedData = await replaceImages(cleanData)
+
+  const compatibleData = removeUnsupportedFeatures(imageConvertedData)
+
+  return { title, date, tags, compatibleData }
+}
+
+const replaceImages = async (content) => {
+  // If image is hosted somewhere else, download it and save it to the public/static folder
+  // Replace markdown image with the new path
+  // Example:
+  // ![cookies](https://i.imgur.com/LE0YpNo.png)
+  // ![cookies](/static/images/cookies.png)
+
+  // If image is hosted on the same server, just replace the path
+  // Example:
+  // ![image](/images/NextDNS-Default-Configuration-Privacy.png)
+  // ![image](/static/images/NextDNS-Default-Configuration-Privacy.png)
+
+  // Regular expression to match markdown image syntax
+  const imageRegex = /!\[([^\]]*)\]\((https?:\/\/[^\s]+)?(\/[^\s]+)?\)/g
+  let contentToReturn = content
+
+  let match
+  while ((match = imageRegex.exec(content))) {
+    const altText = match[1]
+    const externalUrl = match[2]
+    const localPath = match[3]
+
+    if (externalUrl) {
+      // Download and save the external image
+      try {
+        const response = await axios.get(externalUrl, { responseType: 'arraybuffer' })
+        const filename = path.basename(new URL(externalUrl).pathname)
+        // Create new filename of the form altText.extension
+        const newFilename = `${altText}.${filename.split('.').pop()}`
+        const localFilePath = path.join('public', 'static', 'images', newFilename)
+
+        await fs.writeFile(localFilePath, response.data)
+
+        // Replace in content
+        const newImageMarkdown = `![${altText}](/static/images/${newFilename})`
+        contentToReturn = contentToReturn.replace(match[0], newImageMarkdown)
+      } catch (error) {
+        console.error(`Error downloading image ${externalUrl}:`, error.message)
+      }
+    } else if (localPath) {
+      // Copy the image to the ${PWD}/static/ folder
+      await fs.cp(
+        path.join(jekyllRepositoryPath, localPath),
+        path.join('public', 'static', localPath)
+      )
+
+      // Modify the local path
+      const newLocalPath = localPath.replace('/images/', '/static/images/')
+      const newImageMarkdown = `![${altText}](${newLocalPath})`
+      contentToReturn = contentToReturn.replace(match[0], newImageMarkdown)
+    }
+  }
+
+  return contentToReturn
+}
+
+const removeUnsupportedFeatures = (content) => {
+  let contentToReturn = content
+
+  // Remove {:target="_blank"}
+  const regex = /{:target="_blank"}/g
+  contentToReturn = contentToReturn.replace(regex, '')
+
+  return contentToReturn
+}
+
+export default migrateFromJekyll
