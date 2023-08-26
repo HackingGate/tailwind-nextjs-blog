680edf65b94bead12bb8a6be1d0cb9547ea68766
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-08-26 18:00:13 +0900

    Update projectsData

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,10 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'PDFReader',
+    description: `iOS app integrates with the Files app to provide an easy way to read PDFs.
+    With experimental support of RTL reading experience for Arabic, Hebrew, and RTL CJK languages.`,
+    imgSrc: '/static/images/PDFReader/Cover.png',
+    href: '/blog/a-pdf-reader-app-for-iOS-11',
   },
 ]
 
