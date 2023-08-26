8b528469d5df344ed7f71e228989e4a453798d3c
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-08-26 11:29:18 +0900

    Refactor script

---
+++ scripts/migrate.mjs
@@ -0,0 +1,7 @@
+import migrateFromJekyll from './migrateFromJekyll.mjs'
+
+async function migrate() {
+  await migrateFromJekyll()
+}
+
+migrate()
--- scripts/migrateFromJekyll.mjs
+++ scripts/migrateFromJekyll.mjs
@@ -17,42 +17,19 @@ const migrateFromJekyll = async () => {
       continue
     }
 
-    const postPath = path.join(jekyllRepositoryPath, '_posts', post)
-    const data = await fs.readFile(postPath, 'utf8')
-
-    // Read form Jekyll frontmatter
-    // Example:
-    // ---
-    // title: "Hello World"
-    // date: 2020-01-01
-    // tag: osx linux docker or categories: jekyll update
-    // ---
-    const title = data.match(/title: (.*)/)[1]
-    const date = `'${post.match(/^\d{4}-\d{2}-\d{2}/)[0]}'`
-    let tag = ''
-    let tagMatch = data.match(/tag: (.*)/)
-    if (tagMatch) {
-      tag = tagMatch[1]
-    } else {
-      tagMatch = data.match(/categories: (.*)/)
-      if (tagMatch) {
-        tag = tagMatch[1]
-      }
-    }
-    // convert tag to array
-    const tags = `[${tag
-      .split(' ')
-      .map((t) => `${t}`)
-      .join(', ')}]`
-
     // Remove YYYY-MM-DD- from the filename and replace extension with .mdx
     const filename = post.replace(/^\d{4}-\d{2}-\d{2}-/, '').replace(/\.[^/.]+$/, '.mdx')
 
-    const cleanData = data.replace(/---\n(.|\n)*---\n/, '')
+    const filepath = path.join(process.cwd(), 'data', 'blog', `${filename}`)
 
-    const imageConvertedData = await replaceImages(cleanData)
+    // If the file already exists, skip it
+    const fileExists = await fs.readFile(filepath).catch(() => false)
+    if (fileExists) {
+      console.log(`File ${filepath} already exists, skipping...`)
+      continue
+    }
 
-    const compatibleData = removeUnsupportedFeatures(imageConvertedData)
+    const { title, date, tags, compatibleData } = await readJekyllPost(post)
 
     const mdx = `---
 title: ${title}
@@ -65,9 +42,6 @@ ${compatibleData}
     const cleanMDX = mdx.replace(/---\n\n/, '---\n')
     console.log(cleanMDX)
 
-    // Save it to the data/blog folder
-    const filepath = path.join(process.cwd(), 'data', 'blog', `${filename}`)
-    console.log(filepath)
     // Save the content to the file
     await fs.writeFile(filepath, mdx)
 
@@ -95,7 +69,7 @@ ${compatibleData}
     redirects.push(redirect)
   }
 
-  console.log('Copy the following to next.config.js')
+  console.log('Append the following to next.config.js')
   const redirectsString = redirects.join(',\n      ')
 
   console.log(`module.exports = {
@@ -107,14 +81,42 @@ ${compatibleData}
 }`)
 }
 
-const removeUnsupportedFeatures = (content) => {
-  let contentToReturn = content
+const readJekyllPost = async (post) => {
+  const postPath = path.join(jekyllRepositoryPath, '_posts', post)
+  const data = await fs.readFile(postPath, 'utf8')
 
-  // Remove {:target="_blank"}
-  const regex = /{:target="_blank"}/g
-  contentToReturn = contentToReturn.replace(regex, '')
+  // Read form Jekyll frontmatter
+  // Example:
+  // ---
+  // title: "Hello World"
+  // date: 2020-01-01
+  // tag: osx linux docker or categories: jekyll update
+  // ---
+  const title = data.match(/title: (.*)/)[1]
+  const date = `'${post.match(/^\d{4}-\d{2}-\d{2}/)[0]}'`
+  let tag = ''
+  let tagMatch = data.match(/tag: (.*)/)
+  if (tagMatch) {
+    tag = tagMatch[1]
+  } else {
+    tagMatch = data.match(/categories: (.*)/)
+    if (tagMatch) {
+      tag = tagMatch[1]
+    }
+  }
+  // convert tag to array
+  const tags = `[${tag
+    .split(' ')
+    .map((t) => `${t}`)
+    .join(', ')}]`
 
-  return contentToReturn
+  const cleanData = data.replace(/---\n(.|\n)*---\n/, '')
+
+  const imageConvertedData = await replaceImages(cleanData)
+
+  const compatibleData = removeUnsupportedFeatures(imageConvertedData)
+
+  return { title, date, tags, compatibleData }
 }
 
 const replaceImages = async (content) => {
@@ -130,42 +132,55 @@ const replaceImages = async (content) => {
   // ![image](/static/images/NextDNS-Default-Configuration-Privacy.png)
 
   // Regular expression to match markdown image syntax
-  const imageRegex = /!\[([^\]]*)\]\((https?:\/\/[^\s]+)?(\/[^\s]+)?\)/g;
-  let contentToReturn = content;
+  const imageRegex = /!\[([^\]]*)\]\((https?:\/\/[^\s]+)?(\/[^\s]+)?\)/g
+  let contentToReturn = content
 
-  let match;
-  while (match = imageRegex.exec(content)) {
-    const altText = match[1];
-    const externalUrl = match[2];
-    const localPath = match[3];
+  let match
+  while ((match = imageRegex.exec(content))) {
+    const altText = match[1]
+    const externalUrl = match[2]
+    const localPath = match[3]
 
     if (externalUrl) {
       // Download and save the external image
       try {
-        const response = await axios.get(externalUrl, { responseType: 'arraybuffer' });
-        const filename = path.basename(new URL(externalUrl).pathname);
+        const response = await axios.get(externalUrl, { responseType: 'arraybuffer' })
+        const filename = path.basename(new URL(externalUrl).pathname)
         // Create new filename of the form altText.extension
-        const newFilename = `${altText}.${filename.split('.').pop()}`;
-        const localFilePath = path.join('public', 'static', 'images', newFilename);
+        const newFilename = `${altText}.${filename.split('.').pop()}`
+        const localFilePath = path.join('public', 'static', 'images', newFilename)
 
-        await fs.writeFile(localFilePath, response.data);
+        await fs.writeFile(localFilePath, response.data)
 
         // Replace in content
-        const newImageMarkdown = `![${altText}](/static/images/${newFilename})`;
-        contentToReturn = contentToReturn.replace(match[0], newImageMarkdown);
+        const newImageMarkdown = `![${altText}](/static/images/${newFilename})`
+        contentToReturn = contentToReturn.replace(match[0], newImageMarkdown)
       } catch (error) {
-        console.error(`Error downloading image ${externalUrl}:`, error.message);
+        console.error(`Error downloading image ${externalUrl}:`, error.message)
       }
     } else if (localPath) {
       // Copy the image to the ${PWD}/static/ folder
-      await fs.cp(path.join(jekyllRepositoryPath, localPath), path.join('public', 'static', localPath));
+      await fs.cp(
+        path.join(jekyllRepositoryPath, localPath),
+        path.join('public', 'static', localPath)
+      )
 
       // Modify the local path
-      const newLocalPath = localPath.replace('/images/', '/static/images/');
-      const newImageMarkdown = `![${altText}](${newLocalPath})`;
-      contentToReturn = contentToReturn.replace(match[0], newImageMarkdown);
+      const newLocalPath = localPath.replace('/images/', '/static/images/')
+      const newImageMarkdown = `![${altText}](${newLocalPath})`
+      contentToReturn = contentToReturn.replace(match[0], newImageMarkdown)
     }
   }
+
+  return contentToReturn
+}
+
+const removeUnsupportedFeatures = (content) => {
+  let contentToReturn = content
+
+  // Remove {:target="_blank"}
+  const regex = /{:target="_blank"}/g
+  contentToReturn = contentToReturn.replace(regex, '')
 
   return contentToReturn
 }
