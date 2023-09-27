6bdf1d52dd485445b11e230282e6c51033a7d3ba
Tong <i@hackinggate.com>
2023-09-28 01:27:57 +0900

    Static (#12)
    * Added output: 'export' in next.config.js
    
    * Removed out headers() from next.config.js
    
    * Changed Image.tsx to use a standard <img> tag
    
    * Removed redirects
    
    Error: Specified "redirects" cannot be used with "output: export". See more info here: https://nextjs.org/docs/messages/export-no-custom-routes
    
    * Revert "Changed Image.tsx to use a standard <img> tag"
    
    This reverts commit f11476b5a168122b42496673b9c1478494a47026.
    
    * Changed to <img/> tag
    
    * Revert "Changed to <img/> tag"
    
    This reverts commit 0a57794e3e338619031583df4b57c9ae8368231e.
    
    * Set images to unoptimized
    
    * Added _redirects for Cloudflare Pages
    
    * Update _redirects using placeholders
    
    * Revert "Update _redirects using placeholders"
    
    This reverts commit 26d6213f4b31de8cbd908fbbae82e27ad231f1f9.
    
    * Fixed /PDFReader/ redirects
    
    * Changed comment provider to disqus
    
    * Revert "Changed comment provider to disqus"
    
    This reverts commit 75cf79ad342c9e4b2bcc289aa520d0d82165791e.
    
    * Removed unused ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -4,56 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// You might need to insert additional domains in script-src if you are using external services
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
-  connect-src *;
-  font-src 'self';
-  frame-src giscus.app
-`
-
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-]
-
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
@@ -66,16 +16,9 @@ module.exports = () => {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
-      domains: ['picsum.photos'],
+      unoptimized: true,
     },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
+    output: 'export',
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
@@ -83,110 +26,6 @@ module.exports = () => {
       })
 
       return config
-    },
-    async redirects() {
-      return [
-        {
-          source: '/PDFReader',
-          destination: '/blog/a-pdf-reader-app-for-iOS-11',
-          permanent: true,
-        },
-        {
-          source: '/2015/08/31/remap-keys-in-os-x-tutorial-for-emacs-lovers.html',
-          destination: '/blog/remap-keys-in-os-x-tutorial-for-emacs-lovers',
-          permanent: true,
-        },
-        {
-          source: '/2015/09/03/xcode-project-build-number-use-svn-revision.html',
-          destination: '/blog/xcode-project-build-number-use-svn-revision',
-          permanent: true,
-        },
-        {
-          source: '/2015/09/16/helloworld-docker-on-osx-without-boot2docker.html',
-          destination: '/blog/helloworld-docker-on-osx-without-boot2docker',
-          permanent: true,
-        },
-        {
-          source: '/2015/10/01/how-to-download-xcode-with-aria2c.html',
-          destination: '/blog/how-to-download-xcode-with-aria2c',
-          permanent: true,
-        },
-        {
-          source: '/2018/01/24/started-to-learn-korean.html',
-          destination: '/blog/started-to-learn-korean',
-          permanent: true,
-        },
-        {
-          source: '/2019/03/31/macos-mojave-s-dark-mode-for-jekyll-default-theme-minima.html',
-          destination: '/blog/macos-mojave-s-dark-mode-for-jekyll-default-theme-minima',
-          permanent: true,
-        },
-        {
-          source: '/2019/06/09/deploy-jekyll-blog-on-gitlab-pages-with-cloudflare-origin-ca.html',
-          destination: '/blog/deploy-jekyll-blog-on-gitlab-pages-with-cloudflare-origin-ca',
-          permanent: true,
-        },
-        {
-          source: '/2019/06/10/resign-ipas-with-fastlane-for-jailbreak.html',
-          destination: '/blog/resign-ipas-with-fastlane-for-jailbreak',
-          permanent: true,
-        },
-        {
-          source: '/2019/06/11/inspect-the-view-hierarchy-of-any-ios-apps-on-ios-12.html',
-          destination: '/blog/inspect-the-view-hierarchy-of-any-ios-apps-on-ios-12',
-          permanent: true,
-        },
-        {
-          source: '/2019/06/12/apps-fail-to-get-wi-fi-info-on-ios-13.html',
-          destination: '/blog/apps-fail-to-get-wi-fi-info-on-ios-13',
-          permanent: true,
-        },
-        {
-          source: '/2019/06/24/upgrade-to-raspbian-buster-debian-10-for-raspberry-pi.html',
-          destination: '/blog/upgrade-to-raspbian-buster-debian-10-for-raspberry-pi',
-          permanent: true,
-        },
-        {
-          source: '/2019/07/09/using-gpg-to-encrypt-messages.html',
-          destination: '/blog/using-gpg-to-encrypt-messages',
-          permanent: true,
-        },
-        {
-          source: '/2019/07/10/gnu-linux-tar-command-cheat-sheet.html',
-          destination: '/blog/gnu-linux-tar-command-cheat-sheet',
-          permanent: true,
-        },
-        {
-          source: '/2019/07/28/using-aircrack-ng-with-rtl8814au.html',
-          destination: '/blog/using-aircrack-ng-with-rtl8814au',
-          permanent: true,
-        },
-        {
-          source: '/2020/01/25/build-openwrt-19-07-for-tl-wr703n.html',
-          destination: '/blog/build-openwrt-19-07-for-tl-wr703n',
-          permanent: true,
-        },
-        {
-          source: '/2021/03/28/started-to-learn-russian.html',
-          destination: '/blog/started-to-learn-russian',
-          permanent: true,
-        },
-        {
-          source: '/2021/03/28/started-to-learn-russian.html',
-          destination: '/blog/started-to-learn-russian',
-          permanent: true,
-        },
-        {
-          source: '/2021/08/15/network-layer-ip-address-blocking.html',
-          destination: '/blog/network-layer-ip-address-blocking',
-          permanent: true,
-        },
-        {
-          source: '/2021/12/05/moving-to-new-yubikeys.html',
-          destination: '/blog/moving-to-new-yubikeys',
-          permanent: true,
-        },
-      ]
     },
   })
 }
