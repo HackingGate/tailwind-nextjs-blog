bd1ed308d9069e914c052f0c4e5b66ca77746c1f
HackingGate <8541644+HackingGate@users.noreply.github.com>
2023-09-27 10:59:58 +0900

    Revert "Changed Image.tsx to use a standard <img> tag"
    This reverts commit f11476b5a168122b42496673b9c1478494a47026.

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
-import { ImageProps } from 'next/image'
+import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <img {...rest} />
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
